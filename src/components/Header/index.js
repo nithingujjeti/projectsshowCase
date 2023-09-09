e2df7e2e4399bb76566e5191343d99c4fbf3797f
nithin <nithingujjeti77@gmail.com>
2023-09-09 08:51:13 +0000

    solution

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import {HeaderContainer, ImageContainer} from './styledComponents'
+
+const Header = () => (
+  <HeaderContainer>
+    <ImageContainer
+      src="https://assets.ccbp.in/frontend/react-js/projects-showcase/website-logo-img.png"
+      alt="website logo"
+    />
+  </HeaderContainer>
+)
+
+export default Header
---
+++ src/components/Header/styledComponents.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  background-color: #f1f5f9;
+`
+export const ImageContainer = styled.img`
+  margin-top: 15px;
+  margin-bottom: 15px;
+  margin-left: 80px;
+  width: 180px;
+`
